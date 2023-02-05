90b5b887dd5b03ea75ba90e89b737105aeb0a517
Gremlite <gremlite@gmail.com>
2023-02-05 08:19:23 +0200

    Release 1.19.3+build.0 for Forge
    This version introduces some changes to the way NBT data and the
    GossipEntry type is handled, as it is now a record.
    
    Additionally updated the GitHub release workflow as `set-output` is
    being deprecated.

--- src/main/java/ourvillagerdiscounts/ourvillagerdiscounts/eventhandlers/VillagerTradeUpdateEventHandler.java
+++ src/main/java/ourvillagerdiscounts/ourvillagerdiscounts/eventhandlers/VillagerTradeUpdateEventHandler.java
@@ -1,7 +1,11 @@
 package ourvillagerdiscounts.ourvillagerdiscounts.eventhandlers;
 
+import com.mojang.serialization.DataResult;
+import com.mojang.serialization.Dynamic;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.ai.gossip.GossipContainer;
 import net.minecraft.world.entity.ai.gossip.GossipType;
 import net.minecraft.world.entity.npc.Villager;
@@ -10,8 +14,11 @@ import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ourvillagerdiscounts.ourvillagerdiscounts.event.VillagerInteractEvent;
 
+import java.util.Collections;
 import java.util.Comparator;
 
 /**
@@ -19,6 +26,8 @@ import java.util.Comparator;
  */
 @Mod.EventBusSubscriber
 public class VillagerTradeUpdateEventHandler {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     @SubscribeEvent
     public static void updateVillagerTrades(VillagerInteractEvent event) {
         final Villager villager = event.getVillager();
@@ -30,17 +39,18 @@ public class VillagerTradeUpdateEventHandler {
         if (!profession.equals(VillagerProfession.NONE) && !profession.equals(VillagerProfession.NITWIT)) {
             final GossipContainer gossip = villager.getGossips();
             gossip.unpack()
-                    .filter(a -> a.type.equals(GossipType.MAJOR_POSITIVE))
-                    .max(Comparator.comparingInt(a -> a.value))
+                    .filter(a -> a.type().equals(GossipType.MAJOR_POSITIVE))
+                    .max(Comparator.comparingInt(GossipContainer.GossipEntry::value))
                     .ifPresent(maxEntry -> {
                         final int majorPositiveGossipWeighted = maxEntry.weightedValue();
                         final int currentMajorPositiveGossipWeighted = gossip.getReputation(player.getUUID(), g -> g.equals(GossipType.MAJOR_POSITIVE));
 
                         if (majorPositiveGossipWeighted > currentMajorPositiveGossipWeighted) {
-                            final ListTag list = new ListTag();
-                            GossipContainer.GossipEntry entry = new GossipContainer.GossipEntry(player.getUUID(), GossipType.MAJOR_POSITIVE, maxEntry.value);
-                            list.add(entry.store(NbtOps.INSTANCE).getValue());
-                            villager.setGossips(list);
+                            GossipContainer.GossipEntry entry = new GossipContainer.GossipEntry(player.getUUID(), GossipType.MAJOR_POSITIVE, maxEntry.value());
+                            final Tag tag = GossipContainer.GossipEntry.LIST_CODEC.encodeStart(NbtOps.INSTANCE, Collections.singletonList(entry)).getOrThrow(true, (e) -> {
+                                LOGGER.error("Failed to encode gossip data {}", e);
+                            });
+                            villager.setGossips(tag);
                         }
                     });
         }
